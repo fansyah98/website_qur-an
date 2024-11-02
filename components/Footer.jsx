5497aaa4dc68bfaeb6f4585c67fea814ea0d59c5
fansyah98 <fansyahdwi204@gmail.com>
2024-11-02 22:09:58 +0700

    update next js

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="p-3 text-center mb-20">
+        <p>
+          
+          <a href="" target="_blank">
+            Fansyah Dwi Krisandy
+          </a>{' '}
+          {/* untuk Sweet heartnya */}
+        </p>
+      </footer>
+    </>
+  )
+}
+
+// 16/02/2022 16.00 WITA
