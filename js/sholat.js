95e741408444674f0f15e77cb9413c6df38914c2
fansyah98 <fansyahdwi204@gmail.com>
2024-11-02 21:42:47 +0700

    update latih ui dan sistem

--- js/sholat.js
+++ js/sholat.js
@@ -1,6 +1,5 @@
-// Ganti dengan latitude dan longitude Anda
-const latitude = '-6.200000';  
-const longitude = '106.816666'; 
+const latitude = '-6.200000'; // Ganti dengan latitude Anda
+const longitude = '106.816666'; // Ganti dengan longitude Anda
 
 fetch(`https://api.aladhan.com/v1/timings?latitude=${latitude}&longitude=${longitude}`)
     .then(response => response.json())
@@ -8,39 +7,39 @@ fetch(`https://api.aladhan.com/v1/timings?latitude=${latitude}&longitude=${longi
         const timings = data.data.timings;
         const prayerTimesDiv = document.getElementById('prayer-times');
         prayerTimesDiv.innerHTML = `
-            <div class="col">
+            <div class="col-12 col-md-4 mb-3">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Sholat Fajat</h5>
+                        <h5 class="card-title">Sholat Fajr</h5>
                         <p class="time">${timings.Fajr}</p>
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col-12 col-md-4 mb-3">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Sholat Dhuzur</h5>
+                        <h5 class="card-title">Sholat Dhuhur</h5>
                         <p class="time">${timings.Dhuhr}</p>
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col-12 col-md-4 mb-3">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Sholat Ahzar</h5>
+                        <h5 class="card-title">Sholat Ashar</h5>
                         <p class="time">${timings.Asr}</p>
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col-12 col-md-4 mb-3">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Shloat Magrib</h5>
+                        <h5 class="card-title">Sholat Maghrib</h5>
                         <p class="time">${timings.Maghrib}</p>
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col-12 col-md-4 mb-3">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Sholat Isya</h5>
